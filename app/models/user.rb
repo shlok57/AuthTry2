f67f180b6378c37e8118ec5d45bff17dfdb38ca5
Shlok Gandhi <shlok.gandhi@gmail.com>
2016-03-14 13:32:01 +0530

    Based off of happy_seed: base, splash, devise, facebook, googleoauth, api included

---
+++ app/models/user.rb
@@ -0,0 +1,23 @@
+class User < ActiveRecord::Base
+  has_many :identities, dependent: :destroy
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable and :omniauthable
+  devise :omniauthable, :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :trackable
+  def facebook
+    identities.where( :provider => "facebook" ).first
+  end
+
+  def facebook_client
+    @facebook_client ||= Facebook.client( access_token: facebook.accesstoken )
+  end
+
+  def google_oauth2
+    identities.where( :provider => "google_oauth2" ).first
+  end
+
+  def google_oauth2_client
+    @google_oauth2_client ||= GoogleAppsClient.client( google_oauth2 )
+  end
+
+end
